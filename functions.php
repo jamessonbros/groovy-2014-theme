88f1eae6105c47ebf612a265b162570893d8eecc
Jim Jamesson <jim@groovywebdesign.com>
2014-03-20 15:58:41 -0400

    theme updates

--- functions.php
+++ functions.php
@@ -17,3 +17,28 @@ require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
+
+function wc_remove_related_products( $args ) {
+	return array();
+}
+add_filter('woocommerce_related_products_args','wc_remove_related_products', 10);
+
+define('WOOCOMMERCE_USE_CSS', false);
+
+add_action( 'init', 'jk_remove_wc_breadcrumbs' );
+function jk_remove_wc_breadcrumbs() {
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
+
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
+
+/** 
+ * remove on single product panel, "Product Description"
+ * since it already says "Description" on tab.
+ */
+add_filter('woocommerce_product_description_heading',
+'isa_product_description_heading');
+ 
+function isa_product_description_heading() {
+    return '';
+}
--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,6 @@
 /**
  * Custom functions
  */
+
+add_filter('woocommerce_show_page_title', '__return_false');
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
