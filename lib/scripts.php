d1d27f3e93e7d40d504bbe2f4b39cad8458b6807
Jim Jamesson <jim@groovywebdesign.com>
2014-04-02 23:44:08 -0400

    updates

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7a07f6304371b10e7b7589336b52aa9e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '143a87747381f432f77b82f964b46147');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
