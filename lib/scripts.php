80a29e1733aaf245999dfd8d4304ff36eb509f25
Jim Jamesson <jim@groovywebdesign.com>
2014-04-02 22:04:37 -0400

    nav padding fix

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '21d824cfccd687830bec985f2924adef');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ae89fbfc5d16374a32acc6c330b9234d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
