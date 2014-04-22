006d7b7a40b1259f93dcba01178d51bec41704de
Steve Jamesson <steve@groovywebdesign.com>
2014-04-22 18:10:11 -0400

    fix .cart a

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '143a87747381f432f77b82f964b46147');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a56af05f9414ed0ae8fd0cc335453ac5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
